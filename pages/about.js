34a15a10dc2850a6edf9f6778dd82813f3f6d8b1
Hung Bui <hung@quaycreative.com>
2020-09-24 09:28:01 +0700

    Initial version with next.js react reac-dom

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Navbar from '../components/navbar';
+const About = () => (
+    <div>
+        <Navbar></Navbar>
+        <h1>About CryptoPrice</h1>
+        <p>Application to view Bitcoin Price</p>
+    </div>
+)
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Navbar from '../components/navbar';
+
+const Index = () => (
+    <div>
+        <Navbar></Navbar>
+        <h1>Welcome to CryptoPrice</h1>
+    </div>
+)
+
+export default Index;
